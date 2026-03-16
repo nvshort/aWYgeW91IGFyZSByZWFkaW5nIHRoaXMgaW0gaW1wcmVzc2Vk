0cf72442281b4deefb6768ac7fef5d5ae0936bfe
nvshort <nlcs.j5626120@gmail.com>
2026-03-16 17:07:18 +0900

    Update script.js

--- untitled folder/untitled folder1/untitled folder2/untitled folder3/untitled folder4/untitled folder5/untitled folder6/untitled folder7/untitled folder8/untitled folder9/script.js
+++ untitled folder/untitled folder1/untitled folder2/untitled folder3/untitled folder4/untitled folder5/untitled folder6/untitled folder7/untitled folder8/untitled folder9/script.js
@@ -39,15 +39,46 @@ function triggerChaos() {
 }
 
 window.addEventListener("load", () => {
+
   const originalText = document.body.innerText;
 
-  const RealObserver = window.MutationObserver;
+  setTimeout(() => {
 
-  const observer = new RealObserver(() => {
-    if (document.body.innerText !== originalText) {
-      triggerChaos();
-    }
-  });
+    const RealObserver = window.MutationObserver;
+
+    const observer = new RealObserver(() => {
+
+      if (document.body.innerText !== originalText) {
+        triggerChaos();
+      }
+
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      characterData: true,
+      subtree: true
+    });
+
+    const scriptWatcher = new RealObserver(() => {
+
+      const scriptStillExists =
+        document.querySelector('script[src="script.js"]');
+
+      if (!scriptStillExists) {
+        triggerChaos();
+      }
+
+    });
+
+    scriptWatcher.observe(document.head, {
+      childList: true,
+      subtree: true
+    });
+
+  }, 500);
+
+});
 
   observer.observe(document.body, {
     childList: true,
